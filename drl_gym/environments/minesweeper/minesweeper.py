7e3bead74e05e2e9829ddcea55a81d98a8a52235
Alexisptr <alexispetrillo@hotmail.fr>
2020-01-26 16:11:14 +0100

    updated minesweeper to feet with deep q learning (int to float) + cleaned some code

--- drl_gym/environments/minesweeper/minesweeper.py
+++ drl_gym/environments/minesweeper/minesweeper.py
@@ -1,10 +1,27 @@
 import os
 import random
+import hashlib
 from typing import List
-
+import pygame
 import numpy as np
-
 from drl_gym.contracts import GameState
+
+pygame.init()
+COLUMNS = 9
+ROWS = 9
+SQUARE_SIZE = 100
+WIDTH = COLUMNS * SQUARE_SIZE
+HEIGHT = (ROWS + 1) * SQUARE_SIZE
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+DRAWING_DELAY = 500
+
+
+class Cell:
+    EMPTY = -1
+    BOMB = -2
 
 
 class MinesweeperGameState(GameState):
@@ -12,21 +29,13 @@ class MinesweeperGameState(GameState):
         self.game_over = False
         self.scores = np.array([0], dtype=int)
         self.available_actions = [i for i in range(81)]
-        self.board = np.array(
-            [
-                [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-                [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-            ],
-            dtype=int,
-        )  # -2 = bombe, -1 = vide, 0 à 8 = nombre de bombes qui entourent
+        self.board = np.full((9, 9), Cell.EMPTY, dtype=np.float)  # -2 = bombe, -1 = vide, 0 à 8 = nombre de bombes qui entourent
         self.world = np.zeros([9, 9], dtype=np.float)
+
+        # Pygame
+        self.screen = None
+        self.font = None
+
         for n in range(10):
             self.place_bomb(self.world)
 
@@ -40,8 +49,8 @@ class MinesweeperGameState(GameState):
         r = random.randint(0, 8)
         c = random.randint(0, 8)
         current_row = world[r]
-        if not current_row[c] == -2:
-            current_row[c] = -2
+        if not current_row[c] == Cell.BOMB:
+            current_row[c] = Cell.BOMB
         else:
             self.place_bomb(world)
 
@@ -50,33 +59,33 @@ class MinesweeperGameState(GameState):
         if rn - 1 > -1:
             r = world[rn - 1]
             if c - 1 > -1:
-                if not r[c - 1] == -2:
+                if not r[c - 1] == Cell.BOMB:
                     r[c - 1] += 1
             if not r[c] == -2:
                 r[c] += 1
             if 9 > c + 1:
-                if not r[c + 1] == -2:
+                if not r[c + 1] == Cell.BOMB:
                     r[c + 1] += 1
 
         # Same row.
         r = world[rn]
         if c - 1 > -1:
-            if not r[c - 1] == -2:
+            if not r[c - 1] == Cell.BOMB:
                 r[c - 1] += 1
         if 9 > c + 1:
-            if not r[c + 1] == -2:
+            if not r[c + 1] == Cell.BOMB:
                 r[c + 1] += 1
 
         # Row below.
         if 9 > rn + 1:
             r = world[rn + 1]
             if c - 1 > -1:
-                if not r[c - 1] == -2:
+                if not r[c - 1] == Cell.BOMB:
                     r[c - 1] += 1
-            if not r[c] == -2:
+            if not r[c] == Cell.BOMB:
                 r[c] += 1
             if 9 > c + 1:
-                if not r[c + 1] == -2:
+                if not r[c + 1] == Cell.BOMB:
                     r[c + 1] += 1
 
     def l(self, r, c, world):
@@ -115,11 +124,11 @@ class MinesweeperGameState(GameState):
 
         self.available_actions.remove(action_index)
 
-        if potential_cell_type == -2:
+        if potential_cell_type == Cell.BOMB:
             self.game_over = True
         elif potential_cell_type == 0:
             self.reveal(wanted_i, wanted_j)
-        elif np.sum(self.board == -1) == 10:  # Victoire -> fin de jeu
+        elif np.sum(self.board == Cell.EMPTY) == 10:  # Victoire -> fin de jeu
             self.game_over = True
         else:
             self.scores[player_index] += 1
@@ -149,24 +158,65 @@ class MinesweeperGameState(GameState):
         return str_acc
 
     def get_unique_id(self) -> int:
-        acc = 0
-        for i in range(81):
-            acc += (9 ** i) * (self.board[i // 9, i % 9] + 1)
-        return acc
+        return hashlib.sha1(self.get_vectorized_state()).hexdigest()
 
     def get_max_state_count(self) -> int:
-        return 11 ** (9 * 9)  # Nbr état possible d'une case ** (nbr row * nbr columns)
+        return 11 ** 81  # Nbr état possible d'une case ** (nbr row * nbr columns)
 
     def get_action_space_size(self) -> int:
         return len(self.available_actions)
 
     def get_vectorized_state(self) -> np.ndarray:
-        """state_vec = np.zeros(5 * 5 * 8)
-        for i in range(5):
-            for j in range(5):
-                state_vec[i * 5 * 5 + j * 8 + (self.board[i, j] + 1)] = 1
-        return state_vec"""
-        pass
+        return self.board.reshape(81)
 
     def render(self):
+        """if not self.screen and not self.font:
+            self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+            self.font = pygame.font.SysFont("monospace", 75)
+        if self.game_over:
+            label = self.font.render(f"Fin de jeu!", 1, BLACK)
+            self.screen.blit(label, (10, 10))
+        for c in range(COLUMNS):
+            for r in range(ROWS):
+                pygame.draw.rect(
+                    self.screen,
+                    BLUE,
+                    (
+                        c * SQUARE_SIZE,
+                        r * SQUARE_SIZE + SQUARE_SIZE,
+                        SQUARE_SIZE,
+                        SQUARE_SIZE,
+                    ),
+                )
+                pygame.draw.rect(
+                    self.screen,
+                    BLACK,
+                    (
+                        int(c * SQUARE_SIZE + SQUARE_SIZE / 2),
+                        int(r * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE / 2),
+                    ),
+                )
+
+        for c in range(COLUMNS):
+            for r in range(ROWS):
+                if self.board[r][c] == Cell.RED:
+                    pygame.draw.rect(
+                        self.screen,
+                        RED,
+                        (
+                            int(c * SQUARE_SIZE + SQUARE_SIZE / 2),
+                            int(r * SQUARE_SIZE + SQUARE_SIZE / 2) + SQUARE_SIZE,
+                        ),
+                    )
+                elif self.board[r][c] == Cell.YELLOW:
+                    pygame.draw.rect(
+                        self.screen,
+                        YELLOW,
+                        (
+                            int(c * SQUARE_SIZE + SQUARE_SIZE / 2),
+                            int(r * SQUARE_SIZE + SQUARE_SIZE / 2) + SQUARE_SIZE,
+                        ),
+                    )
+        pygame.display.update()
+        pygame.time.delay(DRAWING_DELAY)"""
         print(self)
